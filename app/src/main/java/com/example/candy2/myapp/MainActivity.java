abc9db82b3a618c8f2cf8ffb8cebff449513923f
Candycs <chompunut.so.59@ubu.ac.th>
2017-06-10 13:35:33 +0700

    ...

--- app/src/main/java/com/example/candy2/myapp/MainActivity.java
+++ app/src/main/java/com/example/candy2/myapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.candy2.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.R.attr.duration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ShowText(View view) {
+        String msg ="เป็นคนน่ารัก";
+        Toast toast =Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
